007c967a0b7d59eeb174d80ae62a8f129bc4094b
islamrumon <islamrumon420@gmail.com>
2019-08-18 10:44:58 +0600

    from baki
    baki

--- work_form/test/widget_test.dart
+++ work_form/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:work_form/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Screens());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
