068b0e901dc7ee4f3dbc3f3836a6a382e32470b8
islamrumon <islamrumon420@gmail.com>
2019-10-13 19:46:46 +0600

    ok
    down

--- the_telegraph_flutter/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:the_telegraph_flutter/screens/home_screen.dart';
-
-//void main() => runApp(SettingScreen());
-void main() => runApp(HomeScreen());
-
-
