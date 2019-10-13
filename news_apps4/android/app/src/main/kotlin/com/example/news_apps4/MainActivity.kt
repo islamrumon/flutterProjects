3562f368341dc033142396248efa62028ad7c464
islamrumon <islamrumon420@gmail.com>
2019-10-13 19:43:21 +0600

    ok
    ok

---
+++ news_apps4/android/app/src/main/kotlin/com/example/news_apps4/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.news_apps4
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
